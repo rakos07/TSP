c3bdb3816f56ae5f99c9dca476944329d23b1ff8
rakos07 <rakos07@gmail.com>
2020-11-11 18:12:44 +0100

    aStar completed and TSPMR completed

---
+++ mtsp/main.py
@@ -0,0 +1,39 @@
+from galogic import *
+import matplotlib.pyplot as plt
+import progressbar
+pbar = progressbar.ProgressBar()
+
+# Add Dustbins
+for i in range(numNodes):
+    RouteManager.addDustbin(Dustbin())
+
+random.seed(seedValue)
+yaxis = [] # Fittest value (distance)
+xaxis = [] # Generation count
+
+pop = Population(populationSize, True)
+globalRoute = pop.getFittest()
+print ('Initial minimum distance: ' + str(globalRoute.getDistance()))
+
+
+# Start evolving
+for i in pbar(range(numGenerations)):
+    pop = GA.evolvePopulation(pop)
+    localRoute = pop.getFittest()
+    if globalRoute.getDistance() > localRoute.getDistance():
+        globalRoute = localRoute
+    yaxis.append(localRoute.getDistance())
+    xaxis.append(i)
+
+print ('Global minimum distance: ' + str(globalRoute.getDistance()))
+
+print ('Final Route: ' + globalRoute.toString())
+
+xaxisPoint = []
+yaxisPoint = []
+for i in range(len(globalRoute.base)):
+    xaxisPoint.append(globalRoute.base[i].x)
+    yaxisPoint.append(globalRoute.base[i].y)
+
+plt.plot(xaxis, yaxis, 'r-')
+plt.show()
